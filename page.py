df8d8f01a2bb34107b1bd4584712e4ad5250dca3
Al-x-R <alexander.roshchin85@gmail.com>
2019-12-27 13:29:27 +0200

    modified

--- page.py
+++ page.py
@@ -1,13 +1,11 @@
 import requests
 from bs4 import BeautifulSoup as BS
-import json
 
 session = requests.Session()
-# для обхода блокировки прописываем headers
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
            }
-base_url = 'https://dianet.com.ua'
+base_url = 'https://.....'
 
 req = session.get(base_url, headers=headers)
 
